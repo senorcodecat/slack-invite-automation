ac4323b417e7743984d63ca3e17e2e2b51350f05
senorcodecat <senorcodecat@gmail.com>
2020-03-17 11:14:59 +0100

    Fix. . . again

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ module.exports = {
   //   curl -X POST 'https://YOUR-SLACK-TEAM.slack.com/api/users.admin.invite' \
   //   --data 'email=EMAIL&token=TOKEN&set_active=true' \
   //   --compressed
-  slacktoken: process.env.SLACK_TOKEN || 'xoxp-974096622576-963989704449-977270199175-577af9e3ef1d40465f57ee5522b46589',
+  slacktoken: process.env.SLACK_TOKEN || 'xoxp-974096622576-963989704449-992467086450-9fe1dd8dc0dd08d3544442d4b2eccbc4',
   // an optional security measure - if it is set, then that token will be required to get invited.
   inviteToken: process.env.INVITE_TOKEN || null,
   // an optional security measure - if both are set, then recaptcha will be used.
